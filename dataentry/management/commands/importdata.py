8a4dcf5da50b73934c4f59e6e5ea90512d550f52
Christian <c.sangwa@yahoo.fr>
2024-10-21 21:51:41 +0200

    import data to CSV

--- dataentry/management/commands/importdata.py
+++ dataentry/management/commands/importdata.py
@@ -1,8 +1,35 @@
-from django.core.management import BaseCommand
+from django.core.management import BaseCommand, CommandError
+from django.apps import apps
+import csv
 
 class Command(BaseCommand):
   help = "Import data from CSV file"
 
-  def handle(self, *args, **kwargs):
+  def add_arguments(self, parser):
+    parser.add_argument('file_path', type=str, help="CSV file path")
+    parser.add_argument('model_name', type=str, help="Model name")
 
-    self.stdout.write(self.style.SUCCESS("Data imported successfully"))
+    
+  def handle(self, *args, **kwargs):
+    file_path = kwargs['file_path']
+    model_name = kwargs['model_name'].capitalize()
+
+    model = None
+    for app_confing in apps.get_app_configs():
+      try:
+        model = apps.get_model(app_confing.label,model_name)
+        break #Stop searching once model is found
+      except LookupError:
+        continue # model not found in this app, so continue searching in the next app
+
+    if not model:
+        raise CommandError(f"Model {model_name} not found in any app")
+
+    # search for the model across the project apps
+    with open(file_path , 'r') as file:
+      reader =  csv.DictReader(file)
+
+      for row in reader:
+          model.objects.create(**row)
+
+    self.stdout.write(self.style.SUCCESS("Data imported successfully!"))
